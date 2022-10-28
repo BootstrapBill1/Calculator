01f2ac8760a7e562c255d6b81c4d84b1d3d7cdba
Tanner Lynn <tanner.lynn39@gmail.com>
2022-10-28 12:22:06 -0600

    add logic to buttons and display results

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 const NUMBERS = document.querySelectorAll(".number");
+const FUNCTIONS = document.querySelectorAll(".function");
 const DISPLAY = document.querySelector(".display");
+const AC = document.querySelector(".allClear");
+const C = document.querySelector(".clear");
+const EQUALS = document.querySelector(".eval");
+const DECIMAL = document.querySelector(".decimal");
 
+AC.addEventListener("click", () =>{
+    DISPLAY.textContent = "0";
+})
 
+C.addEventListener("click", () =>{
+    lastIndex = DISPLAY.textContent.lastIndexOf(" ");
+    DISPLAY.textContent = DISPLAY.textContent.substring(0, lastIndex);
 
+})
 
+DECIMAL.addEventListener("click", () =>{
+    DISPLAY.textContent += DECIMAL.textContent;
+})
+
+EQUALS.addEventListener("click", () =>{
+    const ARRAY = DISPLAY.textContent.split(" ");
+    console.log(ARRAY[1] +" "+ ARRAY[0]+ " "+ ARRAY[2]);
+    result = operate(ARRAY[1], parseFloat(ARRAY[0]), parseFloat(ARRAY[2])).toPrecision(2);
+    console.log(result);
+    DISPLAY.textContent = result;
+})
+
+FUNCTIONS.forEach(button =>{
+    button.addEventListener("click", () =>{
+        if(button.classList.contains("multiply")){
+            DISPLAY.textContent += " * ";
+        }
+        else if(button.classList.contains("divide")){
+            DISPLAY.textContent += " / "
+        }
+        else{
+            DISPLAY.textContent += " " + button.textContent + " ";
+        }
+        
+    })
+});
 
 NUMBERS.forEach((button) => {
     button.addEventListener("click", () =>{
-        DISPLAY.textContent += button.textContent;
-        //console.log(button.textContent);
+        if(DISPLAY.textContent[0] == "0" && DISPLAY.textContent[1] != "."){
+            DISPLAY.textContent = button.textContent;
+        }
+        else{
+            DISPLAY.textContent += button.textContent;
+        }
     })
 });
 
